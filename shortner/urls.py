a15e0e237c2d6d391893e7503ad8788403877d13
SDido <bookidodo@gmail.com>
2022-01-26 18:40:44 +0200

    prior deployment

--- shortner/urls.py
+++ shortner/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('create', views.create, name="index"),
+    path('<str:pk>', views.go, name='go')
 ]
